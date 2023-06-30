5cdedb6e93c6d60b7987a5b86e45ee811988c9e1
wongprom <>
2023-06-30 12:19:40 +0200

    bg to pink.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-white shadow dark:bg-gray-800">
+    <nav className="bg-pink shadow dark:bg-gray-800">
       <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
         <a
           href="#"
