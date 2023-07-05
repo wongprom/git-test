be4bd49beb5ecaecbe147e0ae7f7478071c8e28a
wongprom <>
2023-07-05 10:35:22 +0200

    FIX5, squashed commits..

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,21 +8,21 @@ const Navbar = () => {
           href="#"
           className="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
         >
-          home
+          HOME
         </a>
 
         <a
           href="#"
           className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
         >
-          features
+          Features
         </a>
 
         <a
           href="#"
           className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
         >
-          blog
+          BLOG
         </a>
 
         <a
