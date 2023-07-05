0fc960db18757ae0ff8ebcd28313880bf690b3a3
wongprom <>
2023-07-05 10:38:00 +0200

    FIX4, add ME link.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -24,6 +24,12 @@ const Navbar = () => {
         >
           BLOG
         </a>
+        <a
+          href="#"
+          className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+        >
+          ME
+        </a>
 
         <a
           href="#"
